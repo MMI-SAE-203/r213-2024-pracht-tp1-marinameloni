73b482b0125f142f2d0cd206125bba7bf912e091
marinameloni <marina.meloni@edu.univ-fcomte.fr>
2024-04-12 14:19:07 +0200

    verification de l'accordéon

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -5,9 +5,10 @@ const maVar = ref(true)
 </script>
 
 <template>
-  <button class="button1" @click="maVar = false">- Cacher -</button>
-  <button class="button1" @click="maVar = true">- Montrer -</button>
-  <button class="button1" @click="maVar = !maVar">- Bascule -</button>
+  <button class="button1" @click="maVar = false">Cacher</button>
+  <button class="button1" @click="maVar = true">Montrer</button>
+  <button class="button1" @click="maVar = !maVar">Bascule</button>
   <h1 v-show="maVar"
-  class="text-2xl">Que des lasagnes   <img src="/public/favicon.ico" alt="garfield"></h1>
+  class="text-2xl">Des lasagnes...   <img src="/public/favicon.ico" alt="garfield"></h1>
+
 </template>

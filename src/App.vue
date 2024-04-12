37df5c9bdab947479fa34ac25a6b1cf32b0981f3
marinameloni <marina.meloni@edu.univ-fcomte.fr>
2024-04-12 13:49:14 +0200

    menu

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,19 @@ onErrorCaptured((err, instance, info) => {
 
 <template>
   <header>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
-      </ul>
-    </nav>
+    <button
+  aria-controls="mainNav"
+  aria-expanded="true"
+  class="rounded-full border-2 border-red-600 bg-red-300 px-2">
+  menu
+  </button>
+<nav id="mainNav">
+  <ul>
+    <li><RouterLink to="@/pages/index.vue" class="text-red-500 underline"> Accueil </RouterLink></li>
+    <li><RouterLink to="@/pages/accordeon.vue" class="text-red-500 underline"> Accordéon </RouterLink></li>
+    <li><a href="#">item 3</a></li>
+  </ul>
+</nav>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
---
+++ src/pages/accordeon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const maVar = ref(true)
+</script>
+
+<template>
+  <button class="button1" @click="maVar = false">- Cacher -</button>
+  <button class="button1" @click="maVar = true">- Montrer -</button>
+  <button class="button1" @click="maVar = !maVar">- Bascule -</button>
+  <h1 v-show="maVar"
+  class="text-2xl">Que des lasagnes   <img src="/public/favicon.ico" alt="garfield"></h1>
+</template>

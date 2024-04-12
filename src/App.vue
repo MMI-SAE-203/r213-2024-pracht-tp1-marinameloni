5943d7f628562fccecb00ee9dd5e66c730c2d6d6
marinameloni <marina.meloni@edu.univ-fcomte.fr>
2024-04-12 14:06:59 +0200

    menu v-show

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+const menuIsOpen = ref(false);
 </script>
 
 <template>
   <header>
     <button
-  aria-controls="mainNav"
-  aria-expanded="true"
-  class="rounded-full border-2 border-red-600 bg-red-300 px-2">
-  menu
-  </button>
-<nav id="mainNav">
-  <ul>
+      aria-controls="mainNav"
+      aria-expanded="menuIsOpen"
+      class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+      @pointerdown="menuIsOpen = !menuIsOpen">
+      menu
+    </button>
+    <nav id="mainNav" v-show="menuIsOpen">
+      <ul>
         <li><router-link to="/" class="text-red-500 underline"> Accueil </router-link></li>
         <li><router-link to="/accordeon" class="text-red-500 underline"> Accordéon </router-link></li>
-    <li><a href="#">item 3</a></li>
-  </ul>
-</nav>
+        <li><a href="#">item 3</a></li>
+      </ul>
+    </nav>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>

8e04757fd839c4d406c0bd415ffd1f0846d78a7d
marinameloni <marina.meloni@edu.univ-fcomte.fr>
2024-04-12 14:09:00 +0200

    transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onErrorCaptured, ref } from 'vue'
+import { onErrorCaptured, ref, Transition } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
@@ -18,13 +18,20 @@ const menuIsOpen = ref(false);
       @pointerdown="menuIsOpen = !menuIsOpen">
       menu
     </button>
-    <nav id="mainNav" v-show="menuIsOpen">
-      <ul>
-        <li><router-link to="/" class="text-red-500 underline"> Accueil </router-link></li>
-        <li><router-link to="/accordeon" class="text-red-500 underline"> Accordéon </router-link></li>
-        <li><a href="#">item 3</a></li>
-      </ul>
-    </nav>
+    <Transition
+      class="transition-transform duration-1000"
+      enter-from-class="-translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="-translate-x-full"
+    >
+      <nav id="mainNav" v-show="menuIsOpen">
+        <ul>
+          <li><router-link to="/" class="text-red-500 underline"> Accueil </router-link></li>
+          <li><router-link to="/accordeon" class="text-red-500 underline"> Accordéon </router-link></li>
+          <li><a href="#">item 3</a></li>
+        </ul>
+      </nav>
+    </Transition>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
